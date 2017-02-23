b97b3ae40c210ab499ee533cb147c075e3431cc1
Tadas <tadas.bobinas@gmail.com>
2017-02-23 14:43:49 +0200

    Added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:` <button md-raised-button>Button</button>
+`
 })
 export class AppComponent {
   title = 'app works!';
